e06022db5e39bbc0ab053e1896b2071580e0b51b
AAKASH GORADIA <80081102+AakashGoradia@users.noreply.github.com>
2022-11-08 17:13:12 +0530

    init
    aakash

---
+++ src/Pages/Landing1.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Content from '../Components/Content'
+
+const Landing1 = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Content/>
+    </div>
+  )
+}
+
+export default Landing1
---
+++ src/Pages/Landing2.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import OurMission from '../Components/OurMission'
+import OurVision from '../Components/OurVision'
+
+const Landing2 = () => {
+  return (
+    <div>
+      <OurMission/>
+      <OurVision/>
+    </div>
+  )
+}
+
+export default Landing2
