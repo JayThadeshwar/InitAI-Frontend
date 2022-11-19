6eda344c15f26f69d21aebdc07b17541d360922c
Jainil <shahjainilb@gmail.com>
2022-11-19 20:30:43 +0530

    small change

--- src/components/Blogs/Blogpage.js
+++ src/components/Blogs/Blogpage.js
@@ -8,7 +8,7 @@ const Blogpage = () => {
       <div className="grid grid-cols-12">
         <div className="bg-blue-100 col-span-1"></div>
         <div className="bg-blue-200 col-span-8"></div>
-        <div className="bg-blue-100 col-span-3">
+        <div className="bg-gradient-to-b from-[#FFFFFF] to-[#CCE2FC] col-span-3">
             <Rightsection/>
         </div>
 
