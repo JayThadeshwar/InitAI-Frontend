e2dde5e262579a000e9a0b242274c006c1fdeb3d
AAKASH GORADIA <80081102+AakashGoradia@users.noreply.github.com>
2022-11-20 17:05:40 +0530

    responsive bottom navigation

--- src/components/Blogs/Blogpage.js
+++ src/components/Blogs/Blogpage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../Navbar'
+import Bottombar from './Bottombar'
 import Rightsection from './Rightsection'
 import Sidebar from './Sidebar'
 
@@ -8,14 +9,16 @@ const Blogpage = () => {
     <div>
       <Navbar/>
       <div className="grid grid-cols-12">
-        <div className="bg-blue-100 col-span-1">
+        <div className="bg-blue-100 col-span-1 hidden lg:block">
           <Sidebar />
         </div>
         <div className="bg-blue-200 col-span-8"></div>
         <div className="bg-gradient-to-b from-[#FFFFFF] to-[#CCE2FC] col-span-3">
             <Rightsection/>
         </div>
-
+        <div className='lg:hidden'>
+          <Bottombar/>
+        </div>
       </div>
     </div>
     
--- src/components/Blogs/Sidebar.js
+++ src/components/Blogs/Sidebar.js
@@ -2,25 +2,26 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div className="row grid grid-cols-3 border-r-[1px] border-[#E6E6E6] bg-white">
+    <div>
+      <div className="row grid grid-cols-3 border-r-[1px] border-[#E6E6E6] bg-white">
         <div className='lg:col-span-1'>
-                <div className='lg:my-60'>
-                  <button className='lg:mx-8 lg:my-2 hover:bg-gray-200 hover:rounded-full lg:p-2'>
-                    <ion-icon name="home-outline" size="large"></ion-icon>
-                  </button>
-                  <button className='lg:mx-8 lg:my-2 hover:bg-gray-200 hover:rounded-full lg:p-2'>
-                    <ion-icon name="bookmarks-outline" size="large"></ion-icon>
-                  </button>
-                  <button className='lg:mx-8 lg:my-2 hover:bg-gray-200 hover:rounded-full lg:p-2'>
-                    <ion-icon name="add-circle-outline" size="large"></ion-icon>
-                  </button>
-                  <button className='lg:mx-8 lg:my-2 hover:bg-gray-200 hover:rounded-full lg:p-2'>
-                    <ion-icon name="document-outline" size="large"></ion-icon>
-                  </button>
-                </div>
+          <div className='lg:my-60 my-auto'>
+            <button className='lg:mx-8 lg:my-2 m-auto p-auto hover:bg-gray-200 hover:rounded-full lg:p-2'>
+              <ion-icon name="home-outline" size="large"></ion-icon>
+            </button>
+            <button className='lg:mx-8 lg:my-2 m-auto p-auto hover:bg-gray-200 hover:rounded-full lg:p-2'>
+              <ion-icon name="bookmarks-outline" size="large"></ion-icon>
+            </button>
+            <button className='lg:mx-8 lg:my-2 m-auto p-auto hover:bg-gray-200 hover:rounded-full lg:p-2'>
+              <ion-icon name="add-circle-outline" size="large"></ion-icon>
+            </button>
+            <button className='lg:mx-8 lg:my-2 m-auto p-auto hover:bg-gray-200 hover:rounded-full lg:p-2'>
+              <ion-icon name="document-outline" size="large"></ion-icon>
+            </button>
+          </div>
         </div>
+      </div>
     </div>
-  
   )
 }
 
