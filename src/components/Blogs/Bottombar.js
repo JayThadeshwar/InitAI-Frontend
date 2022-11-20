1b1101a094fbf72842d1e5b45150105eff733e4b
AAKASH GORADIA <80081102+AakashGoradia@users.noreply.github.com>
2022-11-20 17:15:29 +0530

    hover changes

--- src/components/Blogs/Bottombar.js
+++ src/components/Blogs/Bottombar.js
@@ -6,19 +6,19 @@ const Bottombar = () => {
       <div class="w-full">
 	    <section id="bottom-navigation" class="block fixed inset-x-0 bottom-0 z-10 bg-white shadow">
 		    <div id="tabs" class="flex justify-between font-[Montserrat]">
-			    <a href="/" class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1">
+			    <a href="/" class="w-full focus:text-teal-500 hover:rounded-full hover:bg-gray-200 justify-center inline-block text-center pt-2 pb-1">
                     <ion-icon name="home-outline" size="large"></ion-icon>
 				    <span class="tab tab-home block text-xs">Home</span>
 			    </a>
-			    <a href="/" class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1">
+			    <a href="/" class="w-full focus:text-teal-500 hover:rounded-full hover:bg-gray-200 justify-center inline-block text-center pt-2 pb-1">
                     <ion-icon name="bookmarks-outline" size="large"></ion-icon>
 				<span class="tab tab-kategori block text-xs">Bookmarks</span>
 			</a>
-			<a href="/" class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1">
+			<a href="/" class="w-full focus:text-teal-500 hover:rounded-full hover:bg-gray-200 justify-center inline-block text-center pt-2 pb-1">
                 <ion-icon name="add-circle-outline" size="large"></ion-icon>
 				<span class="tab tab-explore block text-xs">Add</span>
 			</a>
-			<a href="/" class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1">
+			<a href="/" class="w-full focus:text-teal-500 hover:rounded-full hover:bg-gray-200 justify-center inline-block text-center pt-2 pb-1">
                 <ion-icon name="document-outline" size="large"></ion-icon>
 				<span class="tab tab-whishlist block text-xs">Whishlist</span>
 			</a>
