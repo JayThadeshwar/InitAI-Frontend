ac06823426caa94f21c239b4e5ef6a4cd9b9e74a
AAKASH GORADIA <80081102+AakashGoradia@users.noreply.github.com>
2022-11-20 12:32:08 +0530

    before pull

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Nav from "./Nav";
+import Navbar from "./Navbar";
 import Content from "./Content";
 import OurMission from "./OurMission";
 import OurVision from "./OurVision";
@@ -7,7 +7,7 @@ import OurVision from "./OurVision";
 const Landing1 = () => {
   return (
     <div>
-      <Nav />
+      <Navbar />
       <div className="bg-gradient-to-b from-[#edf5fe] to-[#cee3fc]">
         <Content/>
         <OurMission />
