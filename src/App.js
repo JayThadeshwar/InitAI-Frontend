8aa32b36daa0ed9185a3a6315fde77a452e3a05c
Vardhan <vardhandeepak1234@gmail.com>
2023-01-26 20:14:03 +0530

    Fixed changes

--- src/App.js
+++ src/App.js
@@ -3,36 +3,24 @@ import Blogpage from "./components/Blogs/Blogpage";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Landing from "./components/Home/Landing";
-<<<<<<< HEAD
 import Team from "./components/Team/Teampage";
-=======
 import SingleBlog from "./components/Blogs/SingleBlog";
 import ProjectsPage from "./components/Projects/ProjectsPage";
->>>>>>> 43c5ebc45199fa6044042cd65af6840d97bccca6
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
+
       <div className="App overflow-x-hidden">
         <Navbar />
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/events/blogs" element={<Blogpage />} />
+          <Route path="/events/blogs/all" element={<Blogpage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path={`/events/blogs/:id`} element={<SingleBlog />} />
           <Route path="/ourteam" element={<Team />}></Route>
         </Routes>
       </div>
-=======
-    <div className="App overflow-x-hidden">
-    <Navbar/>
-      <Routes>
-        <Route path="/" element={ <Landing /> } />
-        <Route path="/events/blogs/all" element={ <Blogpage /> } />
-        <Route path="/projects" element={ <ProjectsPage /> } />
-        <Route path={`/events/blogs/:id`} element={ <SingleBlog /> } />
-    </Routes>
-    </div>
->>>>>>> 43c5ebc45199fa6044042cd65af6840d97bccca6
     </>
 
   );
