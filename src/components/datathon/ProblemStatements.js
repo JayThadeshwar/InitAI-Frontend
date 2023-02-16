d9361d37b6e97d8dcf9c00cd0ff918a750b9e343
JayThadeshwar <jaythadeshwar007@gmail.com>
2023-02-16 08:59:13 +0530

    PS

--- src/components/datathon/ProblemStatements.js
+++ src/components/datathon/ProblemStatements.js
@@ -2,9 +2,9 @@ import React from "react";
 import RedVertical from "../../assets/RedVertical.svg";
 import WhiteVertical from "../../assets/WhiteVertical.svg";
 import PSCard from "./ProbStat";
-// import PS1 from '../../assets/PS1.pdf';
-// import PS2 from '../../assets/PS2.pdf';
-// import PS3 from '../../assets/PS3.pdf';
+import PS3 from '../../assets/ps1 wealthy.pdf';
+import PS2 from '../../assets/ps3.pdf';
+import PS1 from '../../assets/ps2 Text summarization.pdf';
 
 const ProblemStatements = () => {
     return (
@@ -18,13 +18,13 @@ const ProblemStatements = () => {
                 <div className="row flex justify-center">                
                     <div className="grid grid-cols-1 md:grid-cols-3 md:mx-40 md:gap-12 text-center">                        
                         <div className="col-span-1 my-2 rounded-lg mx-2">
-                            {/* <PSCard pdfObj={PS1} title="Text Summarization of research paper" content="How effectively can you summarize the contents of the research papers/articles submitted within healthcare domain?" img="https://miro.medium.com/v2/resize:fit:1400/format:webp/1*WIU4_hSjsoQ7L8BTlIqxsA.jpeg"></PSCard> */}
+                            <PSCard pdfObj={PS1} title="Text Summarization of research paper" content="How effectively can you summarize the contents of the research papers/articles submitted within healthcare domain?" img="https://miro.medium.com/v2/resize:fit:1400/format:webp/1*WIU4_hSjsoQ7L8BTlIqxsA.jpeg"></PSCard>
                         </div>
                         <div className="col-span-1 my-2 rounded-lg mx-2">
-                            {/* <PSCard pdfObj={PS2} title="Motivate data science learners" content="Extract quotes and authors of the quotes every week for next few months. Also, generate data science problems." img="https://miro.medium.com/max/894/1*g_PeSLDdU_mRb5nWZFHp2A.jpeg"></PSCard> */}
+                            <PSCard pdfObj={PS2} title="Motivate data science learners" content="Extract quotes and authors of the quotes every week for next few months. Also, generate data science problems." img="https://miro.medium.com/max/894/1*g_PeSLDdU_mRb5nWZFHp2A.jpeg"></PSCard>
                         </div>
                         <div className="col-span-1 my-2 rounded-lg mx-2">
-                            {/* <PSCard pdfObj={PS3} title="Can you make me wealthy?" content="You are wealthy capitalist in the year 2011, you have 100 Crores INR and you want to multiply it in next 10 years." img="https://themoneycog.com/wp-content/uploads/2022/02/coins-stack-leaf-with-line-graph-business-connect-white-background-saving-ideas-investment-budget-business-saving-money-concept-save-money-prepare-future.jpg"></PSCard> */}
+                            <PSCard pdfObj={PS3} title="Can you make me wealthy?" content="You are wealthy capitalist in the year 2011, you have 100 Crores INR and you want to multiply it in next 10 years." img="https://themoneycog.com/wp-content/uploads/2022/02/coins-stack-leaf-with-line-graph-business-connect-white-background-saving-ideas-investment-budget-business-saving-money-concept-save-money-prepare-future.jpg"></PSCard>
                         </div>
                     </div>
 
